96a6ee8469c5ef970ddaa19fd2c59e765b9b0774
Fabian Sanchez <fasaro@gmail.com>
2021-01-09 15:39:24 -0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Magento 2 Spanish (Perú) language pack
+ *
+ * @category   Language
+ * @package    es_PE
+ * @author     fasaro
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'fasaro_es_pe',
+    __DIR__
+);
